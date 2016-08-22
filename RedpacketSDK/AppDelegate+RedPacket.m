c21580a703088cfe585f228ac070f7c97fc496e5
都基鹏 <dylandu123@163.com>
2016-08-22 19:10:33 +0800

    增加AppDelegate分类 处理支付宝回调事件

---
+++ RedpacketSDK/AppDelegate+RedPacket.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+RedPacket.m
+//  ChatKit-OC
+//
+//  Created by 都基鹏 on 16/8/22.
+//  Copyright © 2016年 ElonChan. All rights reserved.
+//
+
+#import "AppDelegate+RedPacket.h"
+
+@implementation AppDelegate (RedPacket)
+
+@end
