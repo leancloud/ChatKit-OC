f0a0b308d8ab979b594e81656993e17e6e25be2f
zhihanhe <zhihanhe@gmail.com>
2019-01-24 14:20:54 +0800

    提交稳定版本

--- ChatKit-OC/Example/AppDelegate.h
+++ ChatKit-OC/Example/AppDelegate.h
@@ -5,7 +5,7 @@
 //  v0.8.5 Created by ElonChan on 16/2/24.
 //  Copyright © 2016年 LeanCloud. All rights reserved.
 //
-
+ 
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
